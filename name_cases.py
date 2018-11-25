4c0d9fa596286b6e1033dda5f1111db854ad527a
Najib El Kihel <najib.elkihel@gmail.com>
2018-11-25 15:06:31 +0000

    Create name_cases.py
    Up to page 29 "Try it yourself".

---
+++ name_cases.py
@@ -0,0 +1,18 @@
+name = 'john'
+message_greetings = "Hello "
+message_conclusion = ", would you like to learn some Python today?"
+print(message_greetings+name.title()+message_conclusion)
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+famous_person = "Wayne Gretzky"
+famous_quote = ' once said: "You miss 100% of the shots you don\'t take"'
+print(famous_person+famous_quote)
+
+athlete1 = " Michael Jordan  "
+print(athlete1.lstrip())
+print(athlete1.rstrip())
+print(athlete1.strip())
+print("Michael \nJordan")
+print("Michael \tJordan")
