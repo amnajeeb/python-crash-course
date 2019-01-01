4697c6c78842f8610209337cdd4acc7eb1f1757b
Najib El Kihel <najib.elkihel@gmail.com>
2019-01-01 18:53:43 +0000

    Stopped at Using int() to Accept... p.119

---
+++ Chapter 7 User Input and While Loops - Lessons/greeter.py
@@ -0,0 +1,8 @@
+name = input('Please enter your name: ')
+print('Hello, ' + name.title() + '!')
+
+prompt = 'If you tell us who you are, we can personalise messages you see.'
+prompt += '\nWhat is your name? '
+
+name = input(prompt)
+print('Hello, ' + name.title() + '!')
