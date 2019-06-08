c20eafb861f38b464caee6adba1ba3cff9c9854c
Najeeb <najeeb@Najeebs-MacBook-Pro.local>
2019-06-08 22:47:35 +0100

    Testing Git integration on Atom

--- Chapter 2 Variables and Simple Data Types - Lessons/personal_message.py
+++ Chapter 2 Variables and Simple Data Types - Lessons/personal_message.py
@@ -17,3 +17,5 @@ print(quote)
 famous_person = 'Albert Einstein'
 message = ' once said: "A person who never made a mistake never tried anything new."'
 print(famous_person + message)
+
+# Testing git
